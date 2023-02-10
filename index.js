b028f860e6bd30beaba12e45c5f76b73fecc8883
Sophie Dtr <119942370+So-Mina@users.noreply.github.com>
2023-02-10 16:07:42 +0100

    lab done :D

--- index.js
+++ index.js
@@ -15,9 +15,49 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+  .then(async(x) => {
+    
+    const newRecipe = await Recipe.create({
+      title: 'my recipe title',
+      level: 'Easy Peasy',
+      ingredients: ['ingredient1', 'ingredient2'],
+      cuisine: 'cuisine',
+      dishType: 'snack',
+      image: 'https://images.media-allrecipes.com/images/75131.jpg',
+      duration: 05,
+      creator: 'Mina',
+      created: '2023-02-10'
+    }) 
+    console.log(newRecipe.title)
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  .then(async() => {  
+    const allRecipes = await Recipe.insertMany(data)
+
+    allRecipes.forEach(recipe => {
+       console.log(recipe.title)
+      })
+  })
+
+  .then(async() => {
+    await Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 },
+      { new: true }
+    )
+    console.log('Rigatoni updated!')
+  })
+
+  .then(async() => {
+    await Recipe.deleteOne({ name: 'Carrot Cake' })
+    console.log('Carrot Cake removed!')
+  })
+
+  .catch(error => { 
+      console.error('Error connecting to the database', error)
+  })
+
+ .finally(() => {
+    mongoose.connection.close()
+  })
